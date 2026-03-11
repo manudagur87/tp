7e5ea6d4355782a1fa3ec331934d661e6ecda2fc
podles <podlevan@gmail.com>
2026-03-12 01:53:43 +0800

    remove one space char from EXPECTED.TXT

--- src/main/java/medistock/parser/Parser.java
+++ src/main/java/medistock/parser/Parser.java
@@ -37,8 +37,7 @@ public class Parser {
     private static String getArgument(String text, int index1, int... index2) {
         if (index2.length > 0) {
             return text.substring(index1 + 2, index2[0]).trim();
-        }
-        else {
+        } else {
             return text.substring(index1+2).trim();
         }
     }
