a2826645d5f6fde6d6021b7d0832869afe601fa5
Suwei <shresthasuwei@gmail.com>
2026-03-25 16:53:18 +0800

    Full FindCommand implementation

--- src/main/java/medistock/command/FindCommand.java
+++ src/main/java/medistock/command/FindCommand.java
@@ -1,4 +1,38 @@
 package medistock.command;
 
-public class FindCommand {
+import medistock.exception.MediStockException;
+import medistock.inventory.Inventory;
+import medistock.inventory.InventoryItem;
+import medistock.ui.Ui;
+
+import java.util.List;
+
+/**
+ * Command to find a stock item in the inventory.
+ */
+public class FindCommand extends Command {
+    private final String keyword;
+
+    /**
+     * Creates a FindCommand with the specified keyword.
+     *
+     * @param keyword The keyword to search for in task descriptions.
+     */
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    /**
+     * Executes the find command by searching for items matching the keyword
+     * and displaying the results.
+     *
+     * @param inventory The inventory to search in.
+     * @param ui The UI for displaying the search results.
+     * @throws MediStockException If an error occurs during execution.
+     */
+    @Override
+    public void execute(Inventory inventory, Ui ui) throws MediStockException {
+        List<InventoryItem> matchedItems =inventory.findItem(keyword);
+        ui.showFindList(inventory, matchedItems);
+    }
 }
