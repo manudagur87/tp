52cd26698331588d18c5a3a3b310c24f9c33fc8d
Suwei <shresthasuwei@gmail.com>
2026-03-29 21:06:33 +0800

    Add logging to ListCommand

--- src/main/java/medistock/command/ListCommand.java
+++ src/main/java/medistock/command/ListCommand.java
@@ -4,10 +4,14 @@ import medistock.exception.MediStockException;
 import medistock.inventory.Inventory;
 import medistock.ui.Ui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Command to list all items in the inventory.
  */
 public class ListCommand extends Command {
+    private static final Logger logger = Logger.getLogger(ListCommand.class.getName());
 
     /**
      * Executes the list command by displaying all inventory items.
@@ -18,6 +22,10 @@ public class ListCommand extends Command {
      */
     @Override
     public void execute(Inventory inventory, Ui ui) throws MediStockException {
+        logger.log(Level.INFO, "Executing list command");
+        
         ui.showInventoryList(inventory);
+        
+        logger.log(Level.INFO, "List command completed successfully");
     }
 }
